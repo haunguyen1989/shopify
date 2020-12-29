5acd658797200d4bbbc01d2d4bf1f30ff6d3ffef
hau.nguyen <hau.nguyen@isobar.com>
2020-12-29 12:56:01 +0700

    update code

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.prepare().then(() => {
     createShopifyAuth({
       apiKey: SHOPIFY_API_KEY,
       secret: SHOPIFY_API_SECRET_KEY,
-      scopes: ['read_products', 'write_products'],
+      scopes: ['read_products', 'write_products', 'read_fulfillments', 'write_fulfillments', 'read_orders', 'write_orders'],
       async afterAuth(ctx) {
         const { shop, accessToken } = ctx.session;
         ctx.cookies.set("shopOrigin", shop, {
