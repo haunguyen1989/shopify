a342e36fc648c4a12b0d9b0a2e49f811c2605a01
hau.nguyen <hau.nguyen@isobar.com>
2020-12-29 12:48:41 +0700

    update code

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const { default: graphQLProxy } = require('@shopify/koa-shopify-graphql-proxy');
 const { ApiVersion } = require('@shopify/koa-shopify-graphql-proxy');
 const Router = require('koa-router');
 const { receiveWebhook, registerWebhook } = require('@shopify/koa-shopify-webhooks');
-const getSubscriptionUrl = require('./server/getSubscriptionUrl');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
@@ -54,7 +53,8 @@ app.prepare().then(() => {
         } else {
           console.log('Failed to register webhook', registration.result);
         }
-        await getSubscriptionUrl(ctx, accessToken, shop);
+        console.log('afterAuth accessToken:' + accessToken);
+        ctx.redirect('/');
       }
     })
   );
